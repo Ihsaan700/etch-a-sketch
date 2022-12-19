1066784aba0042f7b09821f21357e4d9f20ba14e
ihsaanul hoque <ihsaanulhoque@gmail.com>
2022-12-19 02:16:44 +0000

    drawing point is now random on x and y axis

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 ctx.strokeStyle = '#000000'
 
+// getting the width and height of the canvas to draw from random points
+const width = canvas.width;
+const height = canvas.height;
+
+// using math.random to choose random points to start drawing
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
 // drawing a point in our canvas
 ctx.beginPath();
-ctx.moveTo(200,200);
-ctx.lineTo(200,200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
+
